333557584bd811d5130d43e1012a93536c54f99d
Javier Noris <norisjavier@gmail.com>
2015-04-21 01:21:52 -0700

    styled home page and finished about page

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,7 +1,7 @@
 class Tool < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :tool_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :tool_image, :styles => { :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	do_not_validate_attachment_file_type :tool_image
 
 	validates :name, :description, presence: true
